c0c961f93014cb539cc432affe6dea8bf56beb3b
ArunawayNERD <jpigottmail@gmail.com>
2016-11-10 17:45:40 -0500

    Updated code to work per file instead of as a big data array

--- src/Preprocessing/PercentageChangeCalc.java
+++ src/Preprocessing/PercentageChangeCalc.java
@@ -2,44 +2,102 @@ package Preprocessing;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Ryan on 11/3/2016.
  */
 public class PercentageChangeCalc {
-    public static void read() {
-        String[] id = new String[200];
-        int[] open = new int[200];
-        int[] close = new int[200];
+    public static void read(String dataFolderPath, int numDataFiles, int openIndex, int closeIndex) {
+        File dataFolder = new File(dataFolderPath);
 
-        try {
+        File[] folderFiles = dataFolder.list();
 
-            BufferedReader br = new BufferedReader(new FileReader("data.txt"));
 
-            // read the first line from the text file
-            String fileRead = br.readLine();
+        for (int i = 0; i < numDataFiles; i++) {
+            ArrayList<String> fileLines = new ArrayList<String>();
 
-            for (int i = 0; fileRead != null; i++) {
-                // use string.split to load a string array with the values from each line of
-                // the file, using a comma as the delimiter
-                String[] tokenize = fileRead.split(",");
-                id[i] = tokenize[0];
-                open[i] = Integer.parseInt(tokenize[1]);
-                close[i] = Integer.parseInt(tokenize[2]);
+            String outFilePath = "";
 
-                // read next line before looping
-                // if end of file reached
-                fileRead = br.readLine();
+            String[] parsedString;
+            String updatedFileLine;
+
+            try {
+
+                BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+
+                // read the first line from the text file
+                String fileRead = br.readLine();
+
+                while (fileRead != null) {
+                    parsedString = fileRead.split(",");
+
+                    int open = Integer.parseInt(parsedString[openIndex]);
+                    int close = Integer.parseInt(parsedString[closeIndex]);
+
+                    int change = (close - open) / close;
+
+                    updatedFileLine = fileRead + "," + change + "\n";
+
+                    fileLines.add(updatedFileLine);
+                }
+
+                writeToDataFile(outFilePath, fileLines);
+
+
+
+                /*for (int i = 0; fileRead != null; i++) {
+                    // use string.split to load a string array with the values from each line of
+                    // the file, using a comma as the delimiter
+                    String[] tokenize = fileRead.split(",");
+                    id[i] = tokenize[0];
+                    open[i] = Integer.parseInt(tokenize[1]);
+                    close[i] = Integer.parseInt(tokenize[2]);
+
+                    // read next line before looping
+                    // if end of file reached
+                    fileRead = br.readLine();
+                } */
+
+                br.close();
+            } catch (FileNotFoundException fnfe) {
+                System.out.println("file not found");
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
             }
-
-            br.close();
-        } catch (FileNotFoundException fnfe) {
-            System.out.println("file not found");
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
         }
     }
+
+    public static void writeToDataFile(String outFilePath, ArrayList<String> fileLines) {
+
+        String fileContents = "";
+        BufferedWriter dataWriter;
+        File outFile;
+
+
+        //construct output string(file contents)
+        for (String s : fileLines) {
+            fileContents += s;
+        }
+
+
+        try {
+            outFile = new File(outFilePath);
+            dataWriter = new BufferedWriter(new FileWriter(outFile));
+
+            if (outFile.exists())
+                outFile.delete();
+
+            outFile.createNewFile();
+
+            dataWriter.write(fileContents);
+            dataWriter.flush();
+        } catch (IOException e) {
+            System.err.println("Error writing stock data for stock ");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
 
     public static void change(int[] open, int[] close) {
         int[] change = new int[200];
