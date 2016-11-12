e5e1ac9dda83a00c97525d7632f7afb37a4166f7
John P <jpigottmail@gmail.com>
2016-11-11 21:10:27 -0500

    Filled in methods for a stock data object

--- src/clustering/StockDataObject.java
+++ src/clustering/StockDataObject.java
@@ -6,5 +6,36 @@ package clustering;
  */
 public class StockDataObject
 {
+    private String stockID;
+    private double openPrice;
+    private double closePrice;
+    private double percentChange;
 
+    public StockDataObject(String stockID, double openPrice, double closePrice, double percentChange)
+    {
+        this.stockID = stockID;
+        this.openPrice = openPrice;
+        this.closePrice = closePrice;
+        this.percentChange = percentChange;
+    }
+
+    public String getStockID()
+    {
+        return stockID;
+    }
+
+    public double getOpenPrice()
+    {
+        return openPrice;
+    }
+
+    public double getClosePrice()
+    {
+        return closePrice;
+    }
+
+    public double getPercentChange()
+    {
+        return percentChange;
+    }
 }
